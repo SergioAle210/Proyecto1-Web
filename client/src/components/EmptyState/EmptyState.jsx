1cc6936cad4b9bc084062d66f6061b98d5239243
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-02 17:22:22 -0600

    add: EmptyState component

---
+++ client/src/components/EmptyState/EmptyState.jsx
@@ -0,0 +1,19 @@
+import './EmptyState.css'
+
+const EmptyState = () => {
+    return (
+      <div className='styles'>
+        <h2>No hay posts para mostrar</h2>
+        <p>
+          Parece que no hay contenido en este momento. ¡Intenta más tarde!
+        </p>
+        <img
+          src="src/assets/images/NBA.jpg"
+          alt="No hay información"
+          className='imageStyles'
+        ></img>
+      </div>
+    )
+  }
+
+export default EmptyState
