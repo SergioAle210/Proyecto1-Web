4dccbc096f78c72d40122c2e380e9c3955149fb1
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-02 17:00:26 -0600

    add: Footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+      <footer >
+        Copyright 2024 UVG® - By Sergio Orellana
+      </footer>
+    )
+  }
+
+export default Footer
