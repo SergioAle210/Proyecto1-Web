43c9b5e3425372ede51fcb9d5bf55207c7d6b401
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-02 17:48:50 -0600

    add: Blog page

---
+++ client/src/pages/Blog/Blog.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import './Blog.css'
+import Post from '@components/Post'
+import Loading from '@components/Loading'
+import EmptyState from '@components/EmptyState'
+
+const Blog = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('http://127.0.0.1:21122/posts')
+      const data = await response.json()
+      setPosts(data)
+      setIsLoading(false)
+    }
+
+    fetchPosts()
+  }, [])
+
+  if (isLoading) return <Loading />
+  if (!posts.length) return <EmptyState />
+
+  return (
+    <div className="blog">
+      {posts.map(post => (
+        <Post key={post.id} {...post} />
+      ))}
+    </div>
+  )
+}
+
+export default Blog;
