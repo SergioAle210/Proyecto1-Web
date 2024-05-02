9035322d86cfd144a5576c9d1e39e6d533c9e60c
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-01 19:17:24 -0600

    feat: Moved connection to src folder and updated DB connection

--- server/src/conn.js
+++ server/src/conn.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'sergio',
-  database: 'blog_sergio',
+  database: 'basketball_blog_db',
   password: 'serch',
   waitForConnections: true,
   connectionLimit: 10,
