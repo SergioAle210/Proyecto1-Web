a03a14d191c8563dd127269bc234eb11193f746a
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-04 15:29:34 -0600

    feat: Added route handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Blog from '@pages/Blog'
+import Login from '@pages/Login'
+import Admin from '@pages/Admin'
+import PrivateRoute from '@components/PrivateRoute'
+import Nav from '@components/Nav'
 import Header from '@components/Header'
-import Footer from '@components/Footer' 
-import Blog from './pages/Blog'
+import Footer from '@components/Footer'
 
 import './App.css'
 
 function App() {
-  
   return (
-    <div className='app'>
+    <Router>
       <Header />
-      <Blog />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
