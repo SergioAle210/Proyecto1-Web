eeb11d610af69989cdd83340178242e59c11cfa8
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-04 19:11:57 -0600

    feat: Removed React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Blog from '@pages/Blog'
-import Login from '@pages/Login'
-import Admin from '@pages/Admin'
-import PrivateRoute from '@components/PrivateRoute'
-import Nav from '@components/Nav'
+import { NavigationProvider } from '@hooks/useNavigate'
+
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Pages from './pages'
 
 import './App.css'
 
 function App() {
   return (
-    <Router>
+    <NavigationProvider>
       <Header />
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Blog />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>} />
-      </Routes>
+      <Pages />
       <Footer />
-    </Router>
+    </NavigationProvider>
   )
 }
 
