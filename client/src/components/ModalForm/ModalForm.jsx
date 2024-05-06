22506b3946ed6de0246c978b8c29230954f3266c
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-06 16:02:46 -0600

    feat: Improved the form to create or update post form

--- client/src/components/ModalForm/ModalForm.jsx
+++ client/src/components/ModalForm/ModalForm.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
 
 import { useEffect, useState } from 'react'
+import Button from '@components/Button'
+import Input from '@components/Input'
 
 import './ModalForm.css'
 
@@ -25,17 +27,26 @@ const ModalForm = ({ isOpen, onClose, onSubmit, post, isUpdate }) => {
         homeScore: post.home_score || 0,
         awayScore: post.away_score || 0,
         imageUrl: post.image_url || '',
+      });
+    } else {
+      setFormData({
+        title: '',
+        content: '',
+        homeTeam: '',
+        awayTeam: '',
+        homeScore: 0,
+        awayScore: 0,
+        imageUrl: '',
       })
     }
   }, [post])
 
-  const handleChange = (event) => {
-    const { name, value, type } = event.target;
-    const newValue = type === 'number' ? parseInt(value, 10) : value;
+  const handleChange = (name, value) => {
+    // Manejar directamente el cambio de los valores basado en el nombre y valor
     setFormData(prevValues => ({
       ...prevValues,
-      [name]: newValue
-    }));
+      [name]: value
+    }))
   }
 
   const handleSubmit = (e) => {
@@ -51,15 +62,15 @@ const ModalForm = ({ isOpen, onClose, onSubmit, post, isUpdate }) => {
     <div className="modal">
       <div className="modal-content">
         <span className="close-button" onClick={onClose}>&times;</span>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <label>Title: <input type="text" name="title" value={formData.title} onChange={handleChange} required /></label>
-          <label>Content: <textarea name="content" value={formData.content} onChange={handleChange} required /></label>
-          <label>Home Team: <input type="text" name="homeTeam" value={formData.homeTeam} onChange={handleChange} required /></label>
-          <label>Away Team: <input type="text" name="awayTeam" value={formData.awayTeam} onChange={handleChange} required /></label>
-          <label>Home Score: <input type="number" name="homeScore" value={formData.homeScore} onChange={handleChange} required /></label>
-          <label>Away Score: <input type="number" name="awayScore" value={formData.awayScore} onChange={handleChange} required /></label>
-          <label>Image URL: <input type="text" name="imageUrl" value={formData.imageUrl} onChange={handleChange} required /></label>
-          <button type="submit">{isUpdate ? 'Update Post' : 'Create Post'}</button>
+        <form onSubmit={(e) => handleSubmit(e)}>     
+        <Input label="Title" type="text" name="title" value={formData.title} onChange={(value) => handleChange('title', value)} />
+          <Input label="Content" type="textarea" name="content" value={formData.content} onChange={(value) => handleChange('content', value)} />
+          <Input label="Home Team" type="text" name="homeTeam" value={formData.homeTeam} onChange={(value) => handleChange('homeTeam', value)} />
+          <Input label="Away Team" type="text" name="awayTeam" value={formData.awayTeam} onChange={(value) => handleChange('awayTeam', value)} />
+          <Input label="Home Score" type="number" name="homeScore" value={formData.homeScore} onChange={(value) => handleChange('homeScore', value)} />
+          <Input label="Away Score" type="number" name="awayScore" value={formData.awayScore} onChange={(value) => handleChange('awayScore', value)} />
+          <Input label="Image URL" type="text" name="imageUrl" value={formData.imageUrl} onChange={(value) => handleChange('imageUrl', value)} />
+          <Button type="submit" text={isUpdate ? 'Update Post' : 'Create Post'} />
         </form>
       </div>
     </div>
