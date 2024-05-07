02f0ae67110bce2e31e636c0d1d0f3af3b14ff1a
SergioAle210 <ore221122@uvg.edu.gt>
2024-05-07 12:48:21 -0600

    feat: Added eslint-standard to project

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,30 +1,36 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: 'airbnb-base',
+  extends: [
+    'standard',
+    'plugin:react/recommended'
+  ],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: [
-        '.eslintrc.{js,cjs}',
+        '.eslintrc.{js,cjs}'
       ],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
+  plugins: [
+    'react'
+  ],
   rules: {
-    "semi": ["error", "never"],
-    "import/extensions": ["error", "ignorePackages", {
-      "js": "always"
-    }],
-  },
+    semi: ['error', 'never'],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'always'
+    }]
+  }
 }
